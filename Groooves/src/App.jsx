dd81e597e9ae454b6734465d6b5d6d2fa5c2cfd0
Owais <your-email@example.com>
2025-11-27 23:39:07 +0000

    s

---
+++ Groooves/src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { FaInstagram, FaTiktok, FaApple } from "react-icons/fa";
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <img src="/assets/images/logo_no_background.png" className="logo" />
+      <h1>groooves</h1>
+      <p>the music app.</p>
+      <div className="card">
+        <a className="button">
+          <FaApple style={{ marginRight: "0.5em" }} />
+          Download on iOS
+        </a>
+      </div>
+
+      <div className="social-icons">
+        <a href="https://www.instagram.com/grooves.app/" target="_blank" rel="noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://www.tiktok.com/@grooovesapp" target="_blank" rel="noreferrer">
+          <FaTiktok />
+        </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
