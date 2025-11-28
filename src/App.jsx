55499e14603a2a2a1aa9bd3ab2211730dc916884
Owais <your-email@example.com>
2025-11-28 00:31:44 +0000

    Update index, App.jsx, CSS, and Vite config for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,24 @@ function App() {
 
   return (
     <>
+      <img src="/assets/images/logo_no_background.png" className="logo" />
       <h1>groooves</h1>
+      <p>the music app.</p>
+      <div className="card">
+        <a className="button">
+          <FaApple style={{ marginRight: "0.5em" }} />
+          Download on iOS
+        </a>
+      </div>
+
+      <div className="social-icons">
+        <a href="https://www.instagram.com/grooves.app/" target="_blank" rel="noreferrer">
+          <FaInstagram />
+        </a>
+        <a href="https://www.tiktok.com/@grooovesapp" target="_blank" rel="noreferrer">
+          <FaTiktok />
+        </a>
+      </div>
     </>
   );
 }
